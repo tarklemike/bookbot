71d83e0ecbff07475e37a14ed466ee55e033cc76
Mike Tarkleman <mike@Mikes-MacBook-Pro.local>
2025-08-10 22:25:09 +0100

    add get_sorted_dictionarys function

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from stats import get_word_count
-from stats import get_character_count
+from stats import get_word_count,get_character_count, get_sorted_character_dictionarys
 
 def get_book_text(file):
     with open(file) as f:
@@ -10,6 +9,15 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     character_count = get_character_count(text)
-    print(f'{word_count} words found in the document', character_count)
+    character_dictionarys = get_sorted_character_dictionarys(character_count)
+    print("============ BOOKBOT ============")
+    print(f'Analyzing book found at {book_path}')
+    print("----------- Word Count ----------")
+    print(f'Found {word_count} total words')
+    print("--------- Character Count -------")
+    for dictionary in character_dictionarys:
+        if dictionary["char"].isalpha():
+            print(f'{dictionary["char"]}: {dictionary["num"]}')
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,10 @@ def get_character_count(text):
         else:
             count_dictionary[character] = 1
     return count_dictionary
+
+def get_sorted_character_dictionarys(count_dictionary):
+    character_dictionarys =  []
+    for key, value in count_dictionary.items():
+        character_dictionarys.append({"char": key, "num": value})
+    character_dictionarys.sort(reverse=True, key=lambda item: item["num"])
+    return character_dictionarys
